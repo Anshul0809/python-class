a11a590536e2a936236df1fe91e6315a603071cd
Anshul0809 <anshulgajbhiye23@gmail.com>
2025-05-01 19:31:31 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,160 @@
+# import keyword
+# keys=keyword.kwlist
+# keys1=keyword.softkwlist
+# print(keys)
+# print(keys1)
+# print(len(keys))
+# print(len(keys1))
+
+# import string
+# a=string.ascii_letters
+# b=string.ascii_lowercase
+# c=string.ascii_uppercase
+# d=string.punctuation
+# print(a,b,c,d)
+# print(len(a),len(b),len(c),len(d))
+
+# import string
+# abc_xyz01_02=string.ascii_letters
+# print(abc_xyz01_02)
+
+# import string
+# X=string.ascii_letters
+# print(X)
+
+# x=10.5
+# y=20
+# print(x)
+# print(y)
+# print(type(x))
+# print(type(y))
+
+
+# x=10.5
+# y=20+1j
+# print(x)
+# print(y)
+# print(type(x))
+# print(type(y))
+
+
+# ''' x=input("enter your name:")
+#  print (x)
+#  print(id(x))
+#  print(type(x))'''
+
+# x='''python
+# python'''
+# print(x)
+# x='python "class"'
+# print(x)
+
+# l=[10,20,'python','java']
+# print(l)
+# print(type(l))
+# print(id(l))
+
+# t=(10,20,'python',30,'java')
+# print(t)
+# print(type(t))
+# print(id(t))
+
+# d={'name':'anshul','age':18,'qualification':'B.tec'}
+# print(d)
+# print(type(d))
+#print(id(d))
+
+# s={10,20,30,10,10,'python','java'}
+# print(s)
+# print(type(s))
+# print(id(s))
+
+# d={'name':'anshul','age':18,'qualification':'B.tec'}
+# fs=frozenset(d)
+# print(fs)
+# print(type(fs))
+# print(id(fs))
+
+# x,y,z,p,q,r=10,20,30,40,50,60
+# print(x)
+# print(y)
+# print(z)
+# print(p)
+# print(q)
+# print(r)
+
+# x=y=z=r=t=10
+# print(x,y,z,r,t)
+
+# x,y,z=10,2+3j,[2,4,6,8]
+# print(x)
+# print(y)
+# print(z)
+
+# operators
+# x=10.5+2j
+# y=50.2+3j
+# z1=x+y
+# z2=x-y
+# z3=x*y
+# z4=x/y
+# # z5=x//y
+# # z6=x%y
+# z7=x**y
+# print(type(z1))
+# print(type(z2))
+# print(type(z3))
+# print(type(z4))
+# # print(type(z5))
+# # print(type(z6))
+# print(type(z7))
+# x=10
+# x=x+1
+# # x+=1
+# print(x)
+# x=10
+# y=20
+# z=0
+# # x+=y
+# # z+=x
+# z=x+y
+# print(z)
+
+#comparision operator (<,>,==,<=,>=,!=) (boolean)
+
+'''x=10
+y=20
+print(x<y)
+print(x>y)
+print(x==y)
+print(x!=y)
+print(x<=y)
+print(x>=y)
+print(type(x>=y))'''
+
+#logical operator (and,or,not) (boolean)
+'''x=10
+y=20
+z=30
+print(x>y and x>z)
+print(z>x and z>y)'''
+
+#membership operator(in,not in)(boolean)
+''' x='python'
+ print('p' in x)
+ print('P' in x)
+ print('p' not in x)
+ print('P' not in x)
+ print('Ptn' in x)
+ print('th' in x) '''
+
+#identity operator(is, is not)(compair on the basis of address) (boolean)
+
+'''x=10
+y=10
+z=20
+print(id(x))
+print(id(y))
+print(id(z))
+print(x is y)
+print(x is z)'''
